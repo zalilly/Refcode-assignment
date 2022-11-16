3dbc0307e8b15b4e5a2fa89b3667e6273043d168
Sosina (lily) <sosif83@gmail.com>
2022-11-16 18:39:02 -0500

    update website

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-function classToggle() {
-  const navs = document.querySelectorAll('.Navbar__Items')
-  
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-}
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
 
-document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+    /*var logo = document.getElementById('logo');
+    logo.height = 25;
+    logo.width = 25;*/
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+    /*var logo = document.getElementById('logo');
+    logo.height = 35;
+    logo.width = 35;*/
+  }
+}
